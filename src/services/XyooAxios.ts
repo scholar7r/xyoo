3f2e63a6487bbd0ff01bf217d439519ba598b684
Scholar 7r <scholar7r@outlook.com>
2024-12-27 22:21:14 +0800

    feat(XyooAxios): timeout set to 10s

--- src/services/XyooAxios.ts
+++ src/services/XyooAxios.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 import { Logger } from "tslog";
 
 const logger = new Logger();
-export const xyooAxios = axios.create({});
+export const xyooAxios = axios.create({
+  timeout: 1000 * 10,
+  timeoutErrorMessage: "Timeout when fetch endpoint",
+});
 
 xyooAxios.interceptors.request.use(
   (request) => {
@@ -21,7 +24,7 @@ xyooAxios.interceptors.response.use(
     return response;
   },
   (error) => {
-    logger.error(error);
-    return Promise.reject(error);
+    logger.error(error.data);
+    return Promise.reject(error.data);
   },
 );
